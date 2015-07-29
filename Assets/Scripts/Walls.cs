fe9dc6e95970050d78842faa76951077af10e4fe
João Adriano Freitas <jaafreitas@gmail.com>
2015-07-29 01:02:47 -0300

    Walls are now created in run-time.

---
+++ Assets/Scripts/Walls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walls : MonoBehaviour {
+	void CreateWall(int px, int pz, float sx, float sz) {
+		GameObject wall = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		wall.transform.localPosition = new Vector3 (px, 0, pz);
+		wall.transform.localScale = new Vector3 (sx, 2.0f, sz);
+	}
+
+	void Start () {
+		// North Wall.
+		CreateWall (0, 10, 20.5f, 0.5f);
+		// South Wall.
+		CreateWall (0, -10, 20.5f, 0.5f);
+		// East Wall.
+		CreateWall (10, 0, 0.5f, 20.5f);
+		// West Wall.
+		CreateWall (-10, 0, 0.5f, 20.5f);
+	}	
+}
